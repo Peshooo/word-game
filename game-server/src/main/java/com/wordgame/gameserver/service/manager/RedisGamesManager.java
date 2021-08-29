b37a17a69b3ba249a27367a3ff0cb7d8ffd2b23c
Peshooo <petar.nyagolov.2000@gmail.com>
2021-08-29 14:27:12 +0300

    fix redis race conditions with simple lock

--- game-server/src/main/java/com/wordgame/gameserver/service/manager/RedisGamesManager.java
+++ game-server/src/main/java/com/wordgame/gameserver/service/manager/RedisGamesManager.java
@@ -1,14 +1,13 @@
 package com.wordgame.gameserver.service.manager;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wordgame.gameserver.model.RedisGame;
 import com.wordgame.gameserver.service.gameplay.Game;
 import com.wordgame.gameserver.service.gameplay.StandardGame;
 import com.wordgame.gameserver.service.gameplay.SurvivalGame;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.BoundHashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
 import org.springframework.data.redis.support.collections.DefaultRedisMap;
 import org.springframework.data.redis.support.collections.RedisMap;
 import org.springframework.stereotype.Service;
@@ -18,10 +17,15 @@ import java.util.function.BiFunction;
 @Service
 public class RedisGamesManager implements GamesManager {
     private static final String HASH_NAME = "games";
+    private static final String GAME_LOCK_FORMAT = "%s.game.lock";
 
+    private final RedisConnectionFactory redisConnectionFactory;
+    private final RedisTemplate<String, Object> redisTemplate;
     private final RedisMap<String, RedisGame> redisMap;
 
-    public RedisGamesManager(RedisTemplate<String, Object> redisTemplate) {
+    public RedisGamesManager(RedisTemplate<String, Object> redisTemplate, RedisConnectionFactory redisConnectionFactory) {
+        this.redisTemplate = redisTemplate;
+        this.redisConnectionFactory = redisConnectionFactory;
         BoundHashOperations<String, String, RedisGame> boundHashOperations = redisTemplate.boundHashOps(HASH_NAME);
         redisMap = new DefaultRedisMap<>(boundHashOperations);
     }
@@ -42,20 +46,41 @@ public class RedisGamesManager implements GamesManager {
     @Override
     public void delete(String gameId) {
         redisMap.remove(gameId);
+        redisTemplate.delete(getGameLockName(gameId));
     }
 
     @Override
     public Game perform(String gameId, BiFunction<String, Game, Game> operation) {
-        //while (true) { //TODO: Return this while
-            RedisGame redisGame = redisMap.get(gameId);
-            Game game = toGame(redisGame);
-            Game result = operation.apply(gameId, game);
-            RedisGame redisGameResult = toRedisGame(result);
-            return toGame(redisMap.replace(gameId, redisGameResult));
-//            if (redisMap.replace(gameId, redisGame, redisGameResult)) {
-//                return result;
-//            }
-        //}
+        RedisAtomicInteger gameLock = new RedisAtomicInteger(getGameLockName(gameId), redisConnectionFactory);
+
+        acquireLock(gameLock);
+        try {
+            return performInIsolation(gameId, operation);
+        } finally {
+            releaseLock(gameLock);
+        }
+    }
+
+    private String getGameLockName(String gameId) {
+        return String.format(GAME_LOCK_FORMAT, gameId);
+    }
+
+    private void acquireLock(RedisAtomicInteger lock) {
+        while (!lock.compareAndSet(0, 1)) {
+        }
+    }
+
+    private Game performInIsolation(String gameId, BiFunction<String, Game, Game> operation) {
+        RedisGame redisGame = redisMap.get(gameId);
+        Game game = toGame(redisGame);
+        Game result = operation.apply(gameId, game);
+        RedisGame redisGameResult = toRedisGame(result);
+
+        return toGame(redisMap.replace(gameId, redisGameResult));
+    }
+
+    private void releaseLock(RedisAtomicInteger lock) {
+        lock.set(0);
     }
 
     private RedisGame toRedisGame(Game game) {
@@ -72,8 +97,7 @@ public class RedisGamesManager implements GamesManager {
         return redisGame;
     }
 
-    private Game toGame(RedisGame redisGame)
-    {
+    private Game toGame(RedisGame redisGame) {
         switch (redisGame.getGameMode()) {
             case STANDARD:
                 return new StandardGame(redisGame.getGameId(), redisGame.getNickname(), redisGame.getGameStatus(), redisGame.getScore(), redisGame.getTimeLeftMillis(), redisGame.getWords(), redisGame.getLastUpdateTimestamp());
